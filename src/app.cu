e207847f7b62a58fbbfa54a9f7f6c76d38866319
nan <nancy.t.nahra@gmail.com>
2026-04-07 23:07:17 -0500

    add basic kernel launch & host/device memory management, move vec3 struct into separate header file

--- src/app.cu
+++ src/app.cu
@@ -1,61 +1,77 @@
 #include <cmath>
 #include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include "../vec3.h"
+#include <cuda_runtime.h>
 
-struct vec3 {
-    float x, y, z;
 
-    __host__ __device__ inline
-    vec3() : x(0.0f), y(0.0f), z(0.0f) {}
+__global__ void render(vec3 *fb, int max_x, int max_y){
+    //pixel coords i, j
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
 
-    __host__ __device__ inline
-    vec3(float ix, float iy, float iz) : x(ix), y(iy), z(iz) {}
+    if((i>=max_x) || (j>= max_y)) return;
 
-    __host__ __device__ inline
-    vec3 operator+(float other) const {
-        return vec3(x + other, y + other, z + other);
+    // get index of pixel in the form of 1d array
+    int pixel_idx = j*max_x+i;
+    float r = float(i) / (max_x - 1);
+    float g = float(j) / (max_y - 1);
+    float b = 0.0f;
+
+    // frame buffer array is 1d array of pixels
+    fb[pixel_idx] = vec3(r,g,b);
+    
+
+}
+
+int main(){
+
+    int image_width = 256;
+    int image_height = 256;
+    int num_pixels = image_width * image_height;
+    size_t fb_size = num_pixels * sizeof(vec3);
+
+    // unified memory, allocated frame buffer, device memory
+    vec3 *frame_buffer;
+    cudaError_t err = cudaMallocManaged((void **)&frame_buffer, fb_size);
+        if (err != cudaSuccess) {
+        std::cerr << "CUDA malloc failed " << cudaGetErrorString(err) << std::endl;
+        return 1;
     }
 
-    __host__ __device__ inline
-    vec3 operator-(float other) const {
-        return vec3(x - other, y - other, z - other);
+    int threads_x =16;
+    int threads_y=16;
+    dim3 blocks(image_width/threads_x +1, image_height/threads_y +1);
+    dim3 threads(threads_x, threads_y);
+
+    // LAUNCH KERNEL
+    render<<<blocks, threads>>>(frame_buffer, image_width, image_height);
+
+    err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        std::cerr << "Kernel launch failed: " << cudaGetErrorString(err) << std::endl;
+        cudaFree(frame_buffer);
+        return 1;
     }
 
-    __host__ __device__ inline
-    vec3 operator*(float other) const {
-        return vec3(x * other, y * other, z * other);
+    // GPU finish
+    cudaDeviceSynchronize();
+
+    // output frame buffer as ppm (done with CPU)
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    for(int j = 0; j < image_height; j++) {
+        for(int i = 0; i < image_width; i++) {
+            size_t pixel_index = j*image_width + i;
+
+            int ir = int(255.999 * frame_buffer[pixel_index].x);
+            int ig = int(255.999 * frame_buffer[pixel_index].y);
+            int ib = int(255.999 * frame_buffer[pixel_index].z);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
     }
 
-    __host__ __device__ inline
-    vec3 operator/(float other) const {
-        if (fabsf(other) < 1e-8f) return vec3(0.0f, 0.0f, 0.0f);
-        return vec3(x / other, y / other, z / other);
-    }
-
-    __host__ __device__ inline
-    float dot(const vec3& other) const {
-        return x * other.x + y * other.y + z * other.z;
-    }
-
-    __host__ __device__ inline
-    vec3 cross(const vec3& other) const {
-        return vec3(
-            y * other.z - z * other.y,
-            z * other.x - x * other.z,
-            x * other.y - y * other.x
-        );
-    }
-
-    __host__ __device__ inline
-    float magnitude() const {
-        return sqrtf(x*x + y*y + z*z);
-    }
-
-    __host__ __device__
-    void display() const {
-    #ifdef __CUDA_ARCH__
-        printf("(%f, %f, %f)\n", x, y, z);
-    #else
-        std::cout << "(" << x << ", " << y << ", " << z << ")" << std::endl;
-    #endif
-    }
-};
+    cudaFree(frame_buffer);
+    return 0;
+}
