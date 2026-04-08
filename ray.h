09dfd047360c187c46d2c6b5b4232b0ac3b9b64d
nan <nancy.t.nahra@gmail.com>
2026-04-07 23:07:57 -0500

    create ray struct (currently unused)

---
+++ ray.h
@@ -0,0 +1,17 @@
+#include "vec3.h"
+
+struct ray{
+
+    vec3 orig;
+    vec3 dir;
+
+    __device__ ray(){}
+    __device__ ray(const vec3& origin, const vec3& direction) : orig(origin), dir(direction){}
+
+    __device__ vec3 origin() const {return orig;}
+    __device__ vec3 direction() const{return dir;}
+
+    __device__ vec3 point_at_parameter(float t) const {return (orig+t * dir);}
+
+};
+
